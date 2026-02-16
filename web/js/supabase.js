6b53a06371cbf7978e49e5767fef2243297aa602
CPAIZO <cpaizo.email@gmail.com>
2026-02-16 20:50:03 +0800

    0216

---
+++ web/js/supabase.js
@@ -0,0 +1,4 @@
+// web/js/supabase.js
+const SUPABASE_URL = "https://efkiuxgwlzjftlqxtett.supabase.co";
+const SUPABASE_KEY = "sb_publishable_G2itMMa0hbauxhKrnUJtwA_XDbhELbU";
+const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
